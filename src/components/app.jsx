cc48c456e4ad921112b58ef83344b206f0e45f1d
suraz343 <prajapati.suraj343@gmail.com>
2024-08-24 09:03:03 +0530

    note.jsx file completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import Note from "./note";
 
 function App(){
     return(
         <div>
             <Header />
+            <Note />
             <Footer />
         </div>
     );
